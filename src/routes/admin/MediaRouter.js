106561f3b07d6dcb166863c93b793b69c8640a92
Chung Nguyen Quan <chungnq253@gmail.com>
2018-12-16 15:57:35 +0700

    #19. Config upload file media

--- src/routes/admin/MediaRouter.js
+++ src/routes/admin/MediaRouter.js
@@ -5,8 +5,27 @@ import MediaController from '../../controllers/MediaController';
 
 const MediaRouter   = express.Router();
 const MediaCtlrIns  = new MediaController();
+function fileFilter (req, file, cb) {
+
+  console.log({ req, file });
+
+  // The function should call `cb` with a boolean
+  // to indicate if the file should be accepted
+
+  // To reject this file pass `false`, like so:
+  // cb(null, false)
+
+  // To accept the file pass `true`, like so:
+  // cb(null, true)
+
+  // You can always pass an error if something goes wrong:
+  // cb(new Error('I don\'t have a clue!'))
+
+}
+
 const upload = multer({ 
   dest: 'src/storages/', 
+  fileFilter,
 });
 
 MediaRouter.get('/list-media', MediaCtlrIns.listMedias); 
